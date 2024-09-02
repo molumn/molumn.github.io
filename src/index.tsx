42854b06bed91e1db0ddb20f28e03630760ef03d
molumn - student <student@molumn.me>
2024-09-02 15:30:38 +0900

    FIRST

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(
+  document.body as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+      I'm not on website
+  </React.StrictMode>
+);
